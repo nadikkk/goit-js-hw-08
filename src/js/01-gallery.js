aa901393300bfca0961a9ddf4fc7d92d95bd52f7
nadikkk <109106413+nadikkk@users.noreply.github.com>
2022-11-28 13:12:52 +0200

    add tack1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,14 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const markupGalleryItem = galleryItems.map(({description, original, preview}) => `<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`).join("");
+gallery.innerHTML = markupGalleryItem;
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+let lightbox = new SimpleLightbox('.gallery a', {
+	captionsData: "alt",
+	captionDelay: 250,
+});
