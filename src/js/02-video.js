5918a913a35193a4af7a88da8dfa35dfaa3d0630
nadikkk <109106413+nadikkk@users.noreply.github.com>
2022-11-30 13:13:18 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function() {
+	 console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+	 console.log('title:', title);
+});
+
+const onPlay = function(data) {
+	// data is an object containing properties specific to that event
+	// console.log(typeof data.seconds);
+	localStorage.setItem("videoplayer-current-time", JSON.stringify (data.seconds));
+};
+
+player.on('timeupdate', throttle(() => {onPlay}, 1000));
+// console.log(parsedData);
+ const savedData = localStorage.getItem("videoplayer-current-time");
+const parsedData = JSON.parse(savedData);
+// console.log(parsedData);
+
+player.setCurrentTime(parsedData).then(function(seconds) {
+	// seconds = the actual time that the player seeked to
+}).catch(function(error) {
+	switch (error.name) {
+		 case 'RangeError':
+			  // the time was less than 0 or greater than the video’s duration
+			  break;
+
+		 default:
+			  // some other error occurred
+			  break;
+	}
+});
